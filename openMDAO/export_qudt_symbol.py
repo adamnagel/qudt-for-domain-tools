76d0e6218ab41edcbe6912bfcde0dca6788018d4
Yi <liyizhe20@gmail.com>
2013-11-14 00:08:44 -0600

    done a sample script to convert qudt unit to mdao

--- openMDAO/export_qudt_symbol.py
+++ openMDAO/export_qudt_symbol.py
@@ -1,20 +1,57 @@
+# -*- coding:utf-8 -*-
 import sys
 import os
 sys.path.append(os.path.abspath('../'))
-import qudt4dt
 reload(sys) 
 sys.setdefaultencoding( "utf-8" ) 
+
+
+import re
+import qudt4dt
+bara = qudt4dt.Barbara("http://localhost:3030")
+baseUnit = {u'L':u'm', u'M':u'kg',
+            u'T':u's', u'I':u'A',
+            u'N':u'mol',u'J':u'cd',
+            u'\u0398': u'K', u'U':u'rad'}
+
+def symbol_lex_token(symbolString):
+    if symbolString == '':
+        return []
+    else:
+        token = re.compile(ur'^([LMNJTIU\u0398])-?\d*',re.UNICODE)
+        result = re.match(token,symbolString)
+        point = len(result.group(0))
+        power = result.group(0)[len(result.group(1)):]
+        pair = (result.group(1), power)
+        return symbol_lex_token(symbolString[point:]) + [pair]
+        
+def tokens_to_unit(tokens):
+    r = ''
+    for (i,j) in tokens:
+        if j == '':
+            r += '*{sym}'.format(sym = baseUnit[i])
+        else:
+            r += '*{sym}**{pow}'.format( sym = baseUnit[i], pow =j)
+    return r[1:]
+
 def get_all_unit_symbol():
-    bara = qudt4dt.Barbara("http://localhost:3030")
     unitList = bara.list_all_units()
     result = {}
     for u in unitList:
         symbol = bara.get_SI_symbol(u)
-        vec = bara.get_SI_vector(symbol)
-        result[u] = vec
-    return result
+
+        if not symbol == None:
+            print u
+            si = re.match(r'http://qudt.org/vocab/dimension#Dimension_SI_(.+)$',symbol).group(1)
+            print si
+            print tokens_to_unit(symbol_lex_token(si))
+            print ('\n')
+       # vec = bara.get_SI_vector(symbol)
+       # print vec
 
 if __name__ == '__main__':
-    for (k, v) in get_all_unit_symbol().items():
-        print '%s\n %s\n' %(k,v)
-
+    # print u'\u0398'
+    get_all_unit_symbol()
+    #print tokens_to_unit(symbol_lex_token(u'ϴ-1'))
+   # print bara.get_unit_class('http://qudt.org/vocab/unit#SquareCentimeterMinute')
+   # print bara.get_units_in_same_class('http://qudt.org/vocab/unit#SquareCentimeterMinute')
