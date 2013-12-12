4200faf1310d1d60256a9f907b8d53afc40b6cd2
Adam Nagel <adam.nagel+git@gmail.com>
2013-12-12 03:17:51 -0600

    #16 get started with template generation for unit classes

---
+++ barbara/NET_4_0/GenerateUnitClasses/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenerateUnitClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Assume the database is running
+            // Access the database and query all classes inheriting from (and including) Qudt4dtUnit
+            // For each of these, build a partial class
+            // For each of these, add a statement to Barbara allowing "getting" of an instance of that class
+
+            var uc = new UnitClass();
+            uc.ClassName = "SomeClass";
+            uc.ClassURI = "SomeURI";
+            uc.BaseClass = "SomeBaseClass";
+            uc.Namespace = "SomeNamespace";
+            uc.Properties = new List<string>() { "Prop1", "Prop2" };
+
+            Console.Out.WriteLine(uc.GenerateCode());
+        }
+    }
+}
---
+++ barbara/NET_4_0/GenerateUnitClasses/UnitClassData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenerateUnitClasses
+{
+    public class UnitClass
+    {
+        public String Namespace;
+        public String ClassName;
+        public String ClassURI;
+        public String BaseClass;
+        public List<String> Properties;
+
+        public String GenerateCode()
+        {
+            String properties = "";
+            foreach (var field in Properties)
+            {
+                properties += Environment.NewLine;
+                properties += String.Format(propertyFormat, field);
+            }
+
+            return String.Format(codeFormat, Namespace, ClassURI, ClassName, BaseClass, properties);
+        }
+
+        #region Templates
+        private String codeFormat = @"using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using qudt4dt;
+
+namespace {0}
+{{
+    [ClassUri(@""{1}"")]
+    public class {2} : {3}
+    {{
+        public {2}(Uri identity, Uri endpoint) : base(identity, endpoint)
+        {{
+        }}
+
+        {4}
+    }}
+}}";
+        private String propertyFormat = @"        public String {0};";
+        #endregion
+    }
+}
