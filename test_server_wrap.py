f6f04c2f1f38f85f07b0c3ec838cc1eddc74787b
Adam Nagel <adam.nagel+git@gmail.com>
2014-05-07 09:49:37 -0500

    GH-30 fix up server wrap test. add performance test.

--- test_server_wrap.py
+++ test_server_wrap.py
@@ -5,14 +5,16 @@ import time
 from subprocess import Popen
 import shlex
 import os
+from multiprocessing import Process
+from multiprocessing import Pool
+import time
 
 from qudt4dt import sparql
 import qudt4dt
 import fusekiutils
 import struct
 
-def EnumerateAllUnitIndividuals():
-    server_url = "http://localhost:3030"
+def EnumerateAllUnitIndividuals(server_url):
     __url_query = server_url + '/qudt4dt/query?'
 
     # Fetch all unit individuals
@@ -40,67 +42,90 @@ def EnumerateAllUnitIndividuals():
 
 
 class TestServerWrap(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        # check/start fuseki
-        try:
-            fusekiutils.LaunchFuseki()
-        except Exception:
-            pass
-
-        fp = open(os.devnull, 'w')
-        cls._wrap = Popen(shlex.split('python qudt4dt/server/server_warp.py'), stdout=fp)
-        time.sleep(2)
-
-    @classmethod
-    def tearDownClass(cls):
-        cls._wrap.terminate()
+    pass
 
 
 def test_generator(url):
-    def test(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.connect(('localhost', 3031))
-        #time.sleep(2)
-        unit = url
-        send_len = len(unit)
-        sock.send(struct.pack('!i', send_len) + unit)
-
-        rec_len = sock.recv(struct.calcsize('!i'))
-        rec_len = struct.unpack('!i', rec_len)[0]
-        buf = ''
-        count = 0
-        while count < rec_len:
-            _buf = sock.recv(1024)
-            count += len(_buf)
-            buf += _buf
-        #print buf
-        ins = qudt4dt.qudt_pb2.Unit()
-        ins.ParseFromString(buf)
-        sock.close()
-
+    def test_unit(self):
+        ins = test_url(url)
         self.assertTrue(ins is not None)
 
-    return test
+    return test_unit
+
+
+def test_url(url):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.connect(('localhost', 3031))
+    #time.sleep(2)
+    unit = url
+    send_len = len(unit)
+    sock.send(struct.pack('!i', send_len) + unit)
+
+    rec_len = sock.recv(struct.calcsize('!i'))
+    rec_len = struct.unpack('!i', rec_len)[0]
+    buf = ''
+    count = 0
+    while count < rec_len:
+        _buf = sock.recv(1024)
+        count += len(_buf)
+        buf += _buf
+    #print buf
+    ins = qudt4dt.qudt_pb2.Unit()
+    ins.ParseFromString(buf)
+    sock.close()
+
+    return ins
+
+
+def perf_benchmark_generator(AllUnits):
+    def test_performance(self):
+        p = Pool(5)
+        start_time = time.time()
+
+        p.map(test_url, AllUnits)
+
+        elapsed = time.time() - start_time
+        print "Elapsed: %02d seconds" % elapsed
+        return elapsed
+
+    return test_performance
 
 
 if __name__ == '__main__':
-    test_name = 'test_%s' % 'invalid_unit'
-    test = test_generator('http://www.fakenamespace.org/thing#fakeunit')
-    setattr(TestServerWrap, test_name, test)
+    # Launch fuseki
+    fusekiutils.LaunchFuseki()
 
-    for t in EnumerateAllUnitIndividuals():
-        test_name = 'test_%s' % t
-        test = test_generator(t)
+    # Launch server wrap
+    fp = open(os.devnull, 'w')
+    server_wrap = Popen(shlex.split('python server_warp.py'), cwd="qudt4dt/server/", stdout=fp)
+    time.sleep(2)
+
+    try:
+        test_name = 'test_%s' % 'invalid_unit'
+        test = test_generator('http://www.fakenamespace.org/thing#fakeunit')
         setattr(TestServerWrap, test_name, test)
 
-    unittest.main()
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestServerWrap)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+        server_url = "http://localhost:3030"
 
-    print ""
-    print "FAILURES: "
-    print "=========="
-    for failed in unittest.TestResult.failures:
-        print failed
+        all_units = EnumerateAllUnitIndividuals(server_url)
+        for t in all_units:
+            test_name = 'test_%s' % t
+            test = test_generator(t)
+            setattr(TestServerWrap, test_name, test)
+
+        test_performance_func = perf_benchmark_generator(all_units)
+        setattr(TestServerWrap, "test_performance", test_performance_func)
+
+        unittest.main()
+        suite = unittest.TestLoader().loadTestsFromTestCase(TestServerWrap)
+        unittest.TextTestRunner(verbosity=2).run(suite)
+
+        print ""
+        print "FAILURES: "
+        print "=========="
+        for failed in unittest.TestResult.failures:
+            print failed
+
+    finally:
+        server_wrap.terminate()
